435e2eda2d39a9df0d89acd250f150a190f19d9a
claudio-py <claudio.3777@gmail.com>
2024-08-25 16:07:51 -0300

    fix image bg

--- src/pages/Clinica.jsx
+++ src/pages/Clinica.jsx
@@ -1,12 +1,12 @@
 import './Clinica.css';
-import Button from '../components/Button';
+
 
 
 
 export default function Clinica() {
     return (
         <section id='galeria'>
-            <div className='intro-galeria-wrapper'>
+            <div style={{backgroundImage: 'url(./galeria/img5.jpg)'}} className='intro-galeria-wrapper'>
             <div className="intro-galeria">
             <h2>Um espa<span style={{ fontFamily: 'museo', fontSize: '3rem', color: 'var(--bg-light)' }}>Ç</span>o projetado para o seu bem-estar</h2>
             <div >
