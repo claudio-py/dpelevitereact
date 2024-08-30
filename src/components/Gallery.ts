001aac4652d6101c7766a3f71b4a2735fbcb6f81
claudio-py <claudio.3777@gmail.com>
2024-08-29 23:53:16 -0300

    fixed gallery issues

--- src/components/Gallery.ts
+++ src/components/Gallery.ts
@@ -1,50 +1,28 @@
-// src/components/Gallery.ts
-import { Image } from "react-grid-gallery";
+
+
 
 if (typeof (window as any).global === 'undefined') {
   (window as any).global = window;
 }
 
 
-export interface CustomImage extends Image {
-  original: string;
-}
-
-export const images: CustomImage[] = [
+export const images= [
   {
-    src: "./galeria/img1.jpg",
-    original: "./galeria/img1.jpg",
-    width: 162.5,
-    height: 108.3
+    src: "./galeria/img1.jpg"
   },
   {
-    src: "./galeria/img2.jpg",
-    original: "./galeria/img2.jpg",
-    width: 162.5,
-    height: 108.3
+    src: "./galeria/img2.jpg"
   },
   {
-    src: "./galeria/img3.jpg",
-    original: "./galeria/img3.jpg",
-    width: 162.5,
-    height: 108.3
+    src: "./galeria/img3.jpg"
   },
   {
-    src: "./galeria/img5.jpg",
-    original: "./galeria/img5.jpg",
-    width: 162.5,
-    height: 108.3
+    src: "./galeria/img6.jpg"
   },
   {
-    src: "./galeria/img6.jpg",
-    original: "./galeria/img6.jpg",
-    width: 162.5,
-    height: 108.3
+    src: "./galeria/img5.jpg"
   },
   {
-    src: "./galeria/img8.jpg",
-    original: "./galeria/img8.jpg",
-    width: 162.5,
-    height: 108.3
+    src: "./galeria/img8.jpg"
   }
 ];
--- src/pages/Clinica.tsx
+++ src/pages/Clinica.tsx
@@ -1,10 +1,10 @@
 // src/pages/Clinica.tsx
 import { useState } from "react";
-import { Gallery } from "react-grid-gallery";
 import Lightbox from "react-18-image-lightbox";
 import "react-18-image-lightbox/style.css";
 import "./Clinica.css";
-import { images, CustomImage } from "../components/Gallery";
+
+import { images } from "../components/Gallery"; 
 
 // Fix the global is not defined issue
 if (typeof (window as any).global === 'undefined') {
@@ -35,10 +35,9 @@ export default function Clinica() {
     /* console.log(nextIndex,currentImage); */
     setIndex(nextIndex)
   };
-  const handleClick = (index: number, item: CustomImage) => {
+  const handleClick = (index: number) => {
     /* console.log(index, currentImage,item); */
     setIndex(index)
-   
   };
   return (
     <section id='galeria'>
@@ -54,35 +53,27 @@ export default function Clinica() {
 
       <div className='gallery-itself'>
         <p className="gallery-text" >Na D'PELE não apenas tratamos, mas inspiramos o autocuidado. Investimos na promoção de um estilo de vida saudável, na busca constante pela melhor versão de cada paciente.</p>
-        <Gallery
-          images={images}
-          onClick={handleClick}
-      /*     tileViewportStyle={{
-            border: '1px solid red',
-            marginInline: '5rem',
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            objectPosition: 'center'
-          }} */
-          enableImageSelection={false}
-        />
+     
+
+
+        <div className="gallery">
+          {images.map((img, index) => (
+            <div key={index} className="gallery-item" onClick={() => handleClick(index)}>
+              <img src={img.src} />
+            </div>
+          ))}
         {!!currentImage && (
           <Lightbox
-            mainSrc={currentImage.original}
-            imageTitle={currentImage.caption}
-            mainSrcThumbnail={currentImage.src}
-            nextSrc={nextImage.original}
-            nextSrcThumbnail={nextImage.src}
-            prevSrc={prevImage.original}
-            prevSrcThumbnail={prevImage.src}
+            mainSrc={currentImage.src}
+            nextSrc={nextImage.src}
+            prevSrc={prevImage.src}
             onCloseRequest={handleClose}
             onMovePrevRequest={handleMovePrev}
             onMoveNextRequest={handleMoveNext}
           />
         )}
       </div>
-
+</div>
 
       <div className="maps">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.5492674387438!2d-48.48680228935014!3d-1.4451483985351543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x92a48e98a8e0df07%3A0xaac7be51f07ce13b!2sR.%20Ant%C3%B4nio%20Barreto%2C%20456%20-%20F%C3%A1tima%2C%20Bel%C3%A9m%20-%20PA%2C%2066055-050!5e0!3m2!1spt-BR!2sbr!4v1724893758505!5m2!1spt-BR!2sbr"  style={{border:0,
